698f01ef85b96b5fbf3ab3acfadbbfc6f4e5b594
Maryn Zolnai <zolnaimaryn1421@gmail.com>
2020-07-21 14:32:34 +0200

    added toggle cart

--- src/main/webapp/static/js/cart.js
+++ src/main/webapp/static/js/cart.js
@@ -1,7 +1,8 @@
-(function(){
-
- $("button").click(function () {
-    $(".shopping-cart").toggle(1000);
- })
-
-})();
+function myFunction() {
+    var x = document.getElementById("shopping-cart");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
