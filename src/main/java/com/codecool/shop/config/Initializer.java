3aa473630b97e1a39a2fafdf1f7aacb9de68acd6
noir <tulola14@gmail.com>
2020-07-23 20:53:44 +0200

    Html/css main page

--- src/main/java/com/codecool/shop/config/Initializer.java
+++ src/main/java/com/codecool/shop/config/Initializer.java
@@ -40,9 +40,9 @@ public class Initializer implements ServletContextListener {
         productCategoryDataStore.add(console);
 
         //setting up products and printing it
-        productDataStore.add(new Product("Amazon Fire", 49.9f, "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", tablet, amazon));
-        productDataStore.add(new Product("Lenovo IdeaPad Miix 700", 479, "USD", "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", tablet, lenovo));
-        productDataStore.add(new Product("Amazon Fire HD 8", 89, "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", tablet, amazon));
+        productDataStore.add(new Product("Amazon Fire", 49.9f, "USD", "Amazon tablet with fantastic price.", tablet, amazon));
+        productDataStore.add(new Product("Lenovo IdeaPad", 479, "USD", "Tablet.", tablet, lenovo));
+        productDataStore.add(new Product("Amazon Fire HD 8", 89, "USD", "Amazon's latest Fire HD 8 tablet.", tablet, amazon));
         productDataStore.add(new Product("Nokia 3310", 100,"USD","The Best cellphone in the world",tablet, amazon));
         productDataStore.add(new Product("XBOX One", 200, "USD", "Microsoft Console", console, microsoft));
         productDataStore.add(new Product("PS5", 800, "USD", "Sony console", console, sony));
