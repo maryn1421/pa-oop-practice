c98c12f213040b3daf8cce82149565daeccba150
maryn1421 <zolnaimaryn1421@gmail.com>
2020-07-23 13:35:02 +0200

    added cookies,orders page is only reachable if you logged in

--- src/main/java/com/codecool/shop/controller/Admin.java
+++ src/main/java/com/codecool/shop/controller/Admin.java
@@ -10,6 +10,7 @@ import org.thymeleaf.context.WebContext;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -22,5 +23,22 @@ public class Admin extends HttpServlet {
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         WebContext context = new WebContext(req, resp, req.getServletContext());
         engine.process("product/admin.html", context, resp.getWriter());
+
+
     }
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        String user = request.getParameter("name");
+        String pwd = request.getParameter("password");
+        System.out.println(user);
+        System.out.println(pwd);
+        if (user.equals("admin") && pwd.equals("admin")) {
+            System.out.println("im here");
+            Cookie loginCookie = new Cookie("user", user);
+            loginCookie.setMaxAge(30 * 60);
+            response.addCookie(loginCookie);
+            response.sendRedirect(response.encodeRedirectURL(request.getContextPath() + "/orders"));
+        }
     }
+}
